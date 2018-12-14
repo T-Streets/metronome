3c5762a8c2b39aee64409280c799a851b21e5116
T-Streets <tyler.ceja5@gmail.com>
2018-12-14 14:09:09 -0800

    add input to change bpm

--- src/Components/Metronome.js
+++ src/Components/Metronome.js
@@ -5,13 +5,29 @@ export default class Metronome extends Component {
     constructor(){
         super()
 
-    this.state = {
-        playing: false,
-        count: 0,
-        bpm: 100,
-        beatsPerMeasure: 4,
+        this.state = {
+            playing: false,
+            count: 0,
+            bpm: 100 || '',
+            beatsPerMeasure: 4,
+        }
+        
     }
+
+    handleBpmChange = e => {
+        e.preventDefault()
+        const bpm = e.target.value
+        this.setState({ bpm })
+        console.log(this.state.bpm)
     }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        const bpm = e.target.value
+        this.setState({ bpm })
+        console.log(this.state.bpm)
+    }
+
     render() {
         const { playing, bpm } = this.state
         
@@ -19,7 +35,18 @@ export default class Metronome extends Component {
             <div className='metronome'>
                 <div className='bpm-slider'>
                     {bpm} BPM 
-                    <input type='range' min='60' max='240' value={bpm} />
+                    <input 
+                        type='range' 
+                        min='60' 
+                        max='240' 
+                        value={bpm} 
+                        onChange={this.handleBpmChange}
+                    />
+                    <form className='bpm-input' onSubmit={this.handleSubmit}>
+                        <input type='number' />
+                        <input type='submit' value='Set'/>
+                    </form>
+
                 </div>
                 <button>{playing ? 'Stop' : 'Play'}</button>
                 
